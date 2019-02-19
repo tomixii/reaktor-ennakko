cbf0b4e8dba7158172efef3621796a4011bf23ea
Your Name <you@example.com>
2019-02-19 22:41:17 +0200

    working bar chart

---
+++ frontend/src/components/HorizontalBar.vue
@@ -0,0 +1,66 @@
+/*
+import { Bar } from 'vue-chartjs'
+export default {
+  extends: Bar,
+  props: {
+    chartdata: {
+      type: Object,
+      default: null
+    }
+    options: {
+      type: Object,
+      default: null
+    }
+  },
+  mounted () {
+    this.renderChart(this.chartdata, this.options)
+  }
+}
+Vue.component('bar-chart', {
+	extends: Bar,
+	mixins: [VueChartJs.mixins.reactiveProp],
+	data: function () {
+		return {
+			options: {
+				scales: {
+					yAxes: [{
+						ticks: {
+							beginAtZero: true
+						},
+						gridLines: {
+							display: true
+						}
+					}],
+					xAxes: [{
+						ticks: {
+							beginAtZero: true
+						},
+						gridLines: {
+							display: false
+						}
+					}]
+				},
+				legend: {
+					display: false
+				},
+				tooltips: {
+					enabled: true,
+					mode: 'single',
+					callbacks: {
+						label: function(tooltipItems, data) {
+							return tooltipItems.yLabel;
+						}
+					}
+				},
+				responsive: true,
+				maintainAspectRatio: false,
+				height: 200
+			}
+		}
+	},
+	mounted () {
+		// this.chartData is created in the mixin
+		this.renderChart(this.chartData, this.options)
+	}
+})
+*/
--- frontend/src/components/Top20.vue
+++ frontend/src/components/Top20.vue
@@ -1,36 +1,126 @@
 <template>
-  <div>
-    <p>Top 20</p>
 
-    <p>Finland emissions 2005: {{ emissions }} kt</p>
-    <button @click="getEmissions">get emissions</button>
-  </div>
+    <div class="container">
+      <p>Select year:</p>
+      <select v-model="selected" @change="getEmissions()">
+        <option disabled value="">Select year</option>
+        <option v-for="year in years" >
+          {{ year }}
+        </option>
+      </select>
+      <bar-chart v-if="loaded" :chart-data="chartData" :chart-labels="chartLabels"/>
+    </div>
 </template>
 
 <script>
 import axios from 'axios'
-export default {
-  data () {
+import Vue from 'vue'
+import VueChartJs from 'vue-chartjs'
+
+function range(start, end) {
+  return Array(end - start + 1).fill().map((_, idx) => start + idx)
+}
+
+Vue.component('bar-chart', {
+  extends: VueChartJs.HorizontalBar,
+  mixins: [VueChartJs.mixins.reactiveProp],
+  props: {
+        chartData: {
+          type: Array | Object,
+          required: false
+        },
+        chartLabels: {
+          type: Array,
+          required: true
+        }
+  },
+  data: function () {
     return {
-      emissions: 0
+      options: {
+        scales: {
+          yAxes: [{
+            ticks: {
+              beginAtZero: true
+            },
+            gridLines: {
+              display: false
+            }
+          }],
+          xAxes: [{
+            ticks: {
+              beginAtZero: true
+            },
+            gridLines: {
+              display: true
+            }
+          }]
+        },
+        legend: {
+          display: false
+        },
+        tooltips: {
+          enabled: true,
+          mode: 'single',
+          callbacks: {
+            label: function (tooltipItems, data) {
+              return tooltipItems.xLabel
+            }
+          }
+        },
+        responsive: true,
+        maintainAspectRatio: false,
+        backgroundColor: "#123456"
+      }
     }
+  },
+  mounted () {
+    // this.chartData is created in the mixin
+    this.renderChart({
+          labels: this.chartLabels,
+          datasets: [
+            {
+              data: this.chartData,
+            }
+          ]
+        },this.options)
+
+  }
+})
+
+export default {
+  data: () => ({
+    loaded: false,
+    chartLabels: [],
+    chartData: [],
+    selected: '2014',
+    years: range(1960, 2014)
+  }),
+  async mounted () {
+    this.getEmissions()
   },
   methods: {
     getEmissions () {
-      const path = `http://localhost:5000/api/co2/finland/2007`
+      this.chartData = []
+      this.chartLabels = []
+      this.loaded = false
+      const path = 'http://localhost:5000/api/co2/'+this.selected+'/1/20'
       axios.get(path)
         .then(response => {
-          this.emissions = response.data.value
-          console.log('moi')
-          console.log(response.data)
+          var emissions = response.data
+          if (emissions !== undefined) {
+            for (var i = 0; i < emissions.length; i++) {
+              this.chartLabels.push(emissions[i][0])
+              this.chartData.push(emissions[i][1])
+            }
+            console.log(this.chartData)
+            this.loaded = true
+          }
         })
         .catch(error => {
           console.log(error)
         })
     }
-  },
-  created () {
-    this.getEmissions()
   }
 }
+
 </script>
