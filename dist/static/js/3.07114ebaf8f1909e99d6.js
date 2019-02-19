c974fa8ac2b46f4058f49662a01ff0eef4d35fdf
Your Name <you@example.com>
2019-02-19 23:53:00 +0200

    added vue dependency

---
+++ dist/static/js/3.07114ebaf8f1909e99d6.js
@@ -0,0 +1,2 @@
+webpackJsonp([3],{"2gkD":function(l,e,u){"use strict";Object.defineProperty(e,"__esModule",{value:!0});var n=u("VU/8")(null,null,!1,null,null,null);e.default=n.exports}});
+//# sourceMappingURL=3.07114ebaf8f1909e99d6.js.map
---
+++ dist/static/js/5.2d181af8201173b05bc5.js
@@ -0,0 +1,2 @@
+webpackJsonp([5],{UR89:function(e,t,n){"use strict";Object.defineProperty(t,"__esModule",{value:!0});var l={render:function(){this.$createElement;this._self._c;return this._m(0)},staticRenderFns:[function(){var e=this.$createElement,t=this._self._c||e;return t("div",[t("p",[this._v("Big")])])}]},r=n("VU/8")(null,l,!1,null,null,null);t.default=r.exports}});
+//# sourceMappingURL=5.2d181af8201173b05bc5.js.map
